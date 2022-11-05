34572f95c9f900ce664386245abc134e4ba4c3f8
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-11-05 15:18:15 +0400

    Выполнил lesson26

---
+++ script.js
@@ -0,0 +1,37 @@
+
+function getData() {
+    let users = fetch('db.json');
+    users
+        .then(response => response.json())
+        .then(user => {
+            console.log(user);
+            return sendData(user);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+function sendData(addUser) {
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(addUser),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then(response => response.json())
+        .then(addUser => {
+            console.log(addUser);
+        })
+        .catch(error => {
+            console.log(error);
+
+        });
+}
+getData();
+
+
+
+
